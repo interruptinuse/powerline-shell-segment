25c6d3df22a846b060eef80dfb6b68e18438728b
inty <interruptinuse@gmail.com>
2021-11-28 14:06:10 +0000

    Move .sh.eval to .eval

---
+++ powerlineshell/__init__.py
@@ -0,0 +1,18 @@
+import os
+import powerline
+import powerline.lib.shell
+
+def eval(pl, command, format='%s', highlight_groups=['powerline_shell'], divider_highlight_group='background:divider'):
+    p = os.popen(command)
+    result = p.read().strip()
+
+    if p.close() or len(result) == 0:
+        return None
+
+    return [
+        {
+            'contents': format % result,
+            'highlight_groups': highlight_groups,
+            'divider_highlight_group': divider_highlight_group
+        }
+    ]
